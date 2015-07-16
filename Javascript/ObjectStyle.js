a1646f901d71103a67e9f5ffac2bd14e330605f2
Khushali Desai <khushali.desai@sjsu.edu>
2015-07-16 17:33:47 +0530

    Added Objectstyle.js

---
+++ Javascript/ObjectStyle.js
@@ -0,0 +1,22 @@
+var bob = {
+    firstName: "Bob",
+    lastName: "Jones",
+    phoneNumber: "(650) 777-7777",
+    email: "bob.jones@example.com"
+};
+
+var mary = {
+    firstName: "Mary",
+    lastName: "Johnson",
+    phoneNumber: "(650) 888-8888",
+    email: "mary.johnson@example.com"
+};
+
+var contacts = [bob, mary];
+
+function list() {
+    var contactsLength = contacts.length;
+    for (var i = 0; i < contactsLength; i++) {
+        printPerson(contacts[i]);
+    }
+}
---
+++ Javascript/ObjectStyle2.js
@@ -0,0 +1,31 @@
+var friend={};
+friend.megha={
+name : "megha",
+lastname : "Desai",
+address : ['IIM Road','Ahmedabad','29881']
+};
+
+friend.Aarohi={
+    name : "Aarohi",
+    lastname : "Oza",
+    address : ['Thaltej Road', 'Ahmedabad','23455']
+};
+
+var list=function(obj){
+    for(var temp in obj)
+        console.log(temp);
+
+};
+
+var search=function(name){
+    for (var temp in friend)
+    {
+        if(friend[temp].name===name){
+            console.log(friends[temp]);
+      return friends[temp];
+        }
+    }
+};
+
+list(friend);
+search("Aarohi");
