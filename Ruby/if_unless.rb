3f3cd6563174bc245be1a88d8d7c3c4c19cc3ed7
Khushali Desai <khushali.desai@sjsu.edu>
2015-07-17 12:22:58 +0530

    made ruby folder

---
+++ Ruby/if_unless.rb
@@ -0,0 +1,18 @@
+print "Integer please: "
+user_num = Integer(gets.chomp)
+
+if user_num < 0
+  puts "You picked a negative integer!"
+elsif user_num > 0
+  puts "You picked a positive integer!"
+else
+  puts "You picked zero!"
+end
+
+hungry = false
+
+unless hungry
+  puts "I'm writing Ruby programs!"
+else
+  puts "Time to eat!"
+end
