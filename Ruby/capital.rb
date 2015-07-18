16134fa34dc91878d426a805055aebcf6019201d
Khushali Desai <khushali.desai@sjsu.edu>
2015-07-18 07:10:05 +0530

    created prime and capital

---
+++ Ruby/capital.rb
@@ -0,0 +1,6 @@
+def capitalize(string) 
+  puts "#{string[0].upcase}#{string[1..-1]}"
+end
+
+capitalize("ryan") # prints "Ryan"
+capitalize("jane") # prints "Jane"
---
+++ Ruby/prime.rb
@@ -0,0 +1,16 @@
+def prime(n)
+  puts "That's not an integer." unless n.is_a? Integer
+  is_prime = true
+  for i in 2..n-1
+    if n % i == 0
+      is_prime = false
+    end
+  end
+  if is_prime
+    puts "#{n} is prime!"
+  else
+    puts "#{n} is not prime."
+  end
+end
+
+prime(2)
