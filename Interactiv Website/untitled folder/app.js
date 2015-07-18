194952ec69813e4fe6dff6f9cdbf76eae7e01fa4
Khushali Desai <khushali.desai@sjsu.edu>
2015-07-18 17:47:25 +0530

    app.js perfect

--- Interactiv Website/untitled folder/app.js
+++ Interactiv Website/untitled folder/app.js
@@ -1,3 +1,23 @@
-var main=function(){
-};
+var main = function() {
+  $('.icon-menu').click(function() {
+    $('.menu').animate({
+      left: "0px"
+    }, 200);
+
+    $('body').animate({
+      left: "285px"
+    }, 200);
+  });
+
+
+  $('.icon-close').click(function() {
+    $('.menu').animate({
+      left: "-285px"
+    }, 200);
+
+    $('body').animate({
+      left: "0px"
+    }, 200);
+  });
+}; //do not forget ;
 $(document).ready(main);
