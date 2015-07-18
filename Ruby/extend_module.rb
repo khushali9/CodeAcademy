e203a95822d31857bf95f3200b73c0eae4eac1f8
Khushali Desai <khushali.desai@sjsu.edu>
2015-07-18 07:40:05 +0530

    more ruby examples

---
+++ Ruby/extend_module.rb
@@ -0,0 +1,11 @@
+module ThePresent
+  def now
+    puts "It's #{Time.new.hour > 12 ? Time.new.hour - 12 : Time.new.hour}:#{Time.new.min} #{Time.new.hour > 12 ? 'PM' : 'AM'} (GMT)."
+  end
+end
+
+class TheHereAnd
+  extend ThePresent
+end
+
+TheHereAnd.now
--- Ruby/first_class.rb
+++ Ruby/first_class.rb
@@ -8,3 +8,11 @@ class Language
     puts "I'm #{@name} and I was created by #{@creator}!"
   end
 end
+
+ruby = Language.new("Ruby", "Yukihiro Matsumoto")
+python = Language.new("Python", "Guido van Rossum")
+javascript = Language.new("JavaScript", "Brendan Eich")
+
+ruby.description
+python.description
+javascript.description
